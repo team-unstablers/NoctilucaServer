a3420accfe51d951ca438cf46b4d55c499aa5750
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-11-20 19:40:08 +0900

    initial commit

---
+++ NoctilucaServer/NoctilucaServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoctilucaServerApp.swift
+//  NoctilucaServer
+//
+//  Created by Gyuhwan Park on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct NoctilucaServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SiriusKit/SiriusKit/SiriusKit.swift
@@ -0,0 +1,9 @@
+//
+//  SiriusKit.swift
+//  SiriusKit
+//
+//  Created by Gyuhwan Park on 11/20/25.
+//
+
+import Foundation
+
