ff35630a1659170c7cc513bffbbef1226efba016
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-11-23 22:58:08 +0900

    test(SiriusKit/transport/quic): 인증서 관련 테스트를 건너뛸 수 있도록 한다

--- SiriusKit/SiriusKitTests/transport/quic/KeychainQUICServerIdentityTests.swift
+++ SiriusKit/SiriusKitTests/transport/quic/KeychainQUICServerIdentityTests.swift
@@ -10,7 +10,11 @@ import Testing
 @testable import SiriusKit
 
 struct KeychainQUICServerIdentityTests {
-    @Test
+    
+    /// 자가 서명 인증서를 trust 처리하는 과정에서 사용자 인증 다이얼로그가 뜨므로 테스트를 건너뛸 수 있도록 합니다
+    static let skipTest = true
+    
+    @Test("KeychainQUICServerIdentity가 키체인에 올바르게 항목을 생성할 수 있는가", .disabled(if: skipTest))
     func createsKeychainEntry() async throws {
         let keychain = SRKeychain.shared
         
