9e1817f58b928d064869d737d7effb3d4a6fa731
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-12-12 11:35:17 +0900

    server: agent로써 명시한다

---
+++ NoctilucaServer/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  NoctilucaServer
+//
+//  Created by Gyuhwan Park on 12/12/25.
+//
+
+import Foundation
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        print("Hello, World!")
+        NSApp.setActivationPolicy(.accessory)
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return false
+    }
+    
+    
+}
