b6a0e2024680aaf6a8f5cb545d58c5b893775725
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-12-21 00:05:03 +0900

    siriuskit/transport/quic: stream.id를 UUID로 변경하고 내부 스트림 ID 값과 연결을 끊어버림 (resolves #71)

---
+++ SiriusKit/SiriusKit/UUID+Zero.swift
@@ -0,0 +1,12 @@
+//
+//  UUID+Zero.swift
+//  SiriusKit
+//
+//  Created by Gyuhwan Park on 12/20/25.
+//
+
+import Foundation
+
+extension UUID {
+    static let zero = UUID(uuidString: "00000000-0000-0000-0000-000000000000")!
+}
