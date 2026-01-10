8a689ac20e6bc3833eb027f77bdf32a46689c7b6
Gyuhwan Park★ <unstabler@unstabler.pl>
2026-01-10 12:11:27 +0900

    server/projection: Ulalaca로부터 화면 잠금 관련 유틸리티 이식

---
+++ NoctilucaServer/projection/ScreenLockObserver.swift
@@ -0,0 +1,72 @@
+//
+// Created by Gyuhwan Park on 2023/02/04.
+//
+
+import Foundation
+import Combine
+
+import Quartz
+
+import SiriusKit
+
+class ScreenLockObserver: ObservableObject {
+    static let shared = ScreenLockObserver()
+    
+    private let logger = NoctilucaLogger(category: "projection.ScreenLockObserver")
+    private let notificationCenter = DistributedNotificationCenter.default
+    
+    @Published
+    private(set) var isScreenLocked: Bool = false
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init() {
+        startObserve()
+        forceUpdate()
+    }
+
+    deinit {
+        stopObserve()
+    }
+    
+    func forceUpdate() {
+        let locked = CGSessionUtil.isScreenLocked()
+        if locked != isScreenLocked {
+            logger.debug("forceUpdate: isScreenLocked changed to \(locked)")
+            isScreenLocked = locked
+        }
+    }
+
+    private func startObserve() {
+        notificationCenter.publisher(for: NSNotification.Name("com.apple.screenIsLocked"))
+            .receive(on: RunLoop.main)
+            .sink { [weak self] _ in
+                guard let self else { return }
+                
+                if self.isScreenLocked {
+                    return
+                }
+                
+                self.isScreenLocked = true
+            }
+            .store(in: &cancellables)
+        
+         notificationCenter.publisher(for: NSNotification.Name("com.apple.screenIsUnlocked"))
+            .receive(on: RunLoop.main)
+            .sink { [weak self] _ in
+                guard let self else { return }
+                
+                if !self.isScreenLocked {
+                    return
+                }
+                
+                self.isScreenLocked = false
+            }
+            .store(in: &cancellables)
+    }
+
+    private func stopObserve() {
+        cancellables.forEach { $0.cancel() }
+        cancellables.removeAll()
+    }
+}
