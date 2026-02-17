7af0a86e2037343ae1dbd38d9ed1ecd0d8c345ed
Gyuhwan Park★ <unstabler@unstabler.pl>
2026-02-17 09:38:51 +0900

    client/ui: 우라라 모드 설정 UI 스텁 추가 (비활성)

--- NoctilucaClient/NoctilucaClient/core/ui/session-settings/SecuritySessionSettingsTab.swift
+++ NoctilucaClient/NoctilucaClient/core/ui/session-settings/SecuritySessionSettingsTab.swift
@@ -1,8 +1,30 @@
 import SwiftUI
 
 struct SecuritySessionSettingsTab: View {
+#if false
+    static let KEYSTROKES: [KeyEquivalent] = [
+        .upArrow,
+        .upArrow,
+        .downArrow,
+        .downArrow,
+        .leftArrow,
+        .rightArrow,
+        .leftArrow,
+        .rightArrow,
+        .init("b"),
+        .init("a")
+    ]
+    
+    @State
+    var shouldPresentAdditionalSettings: Bool = false
+    
+    @State
+    var additionalSettingsGuardIndex = 0
+#endif
+
     @Binding
     var sessionSettings: SessionSettings
+    
 
     let scope: SessionSettingsScope
     let contactId: UUID?
@@ -48,8 +70,40 @@ struct SecuritySessionSettingsTab: View {
                 Text(markdown: String(localized: "session-settings.security.protocol", defaultValue: "프로토콜"))
                 Text(markdown: String(localized: "session-settings.security.protocol_desc", defaultValue: "Sirius 프로토콜의 동작 방식을 구성합니다."))
             }
+            
+#if false
+            if shouldPresentAdditionalSettings {
+                /// 인터넷을 자유롭게 사용하지 못하는 사람들에게 도움이 되었으면 좋겠습니다
+                Section {
+                    Toggle(isOn: .constant(false)) {
+                        Text(markdown: String(localized: "session-settings.security.protocol.urara.h3", defaultValue: "ALPN 어나운스 시 'h3'을 대신 사용하기"))
+                        Text(markdown: String(localized: "session-settings.security.protocol.urara.h3.desc", defaultValue: "일부 네트워크 환경에서의 호환성을 개선시킬 수 있을거라 믿고 싶습니다."))
+                    }
+                } header: {
+                    Text(markdown: String(localized: "session-settings.security.protocol.urara.title", defaultValue: "개발용 설정: 麗 (우라라) 모드"))
+                    Text(markdown: String(localized: "session-settings.security.protocol.urara.desc", defaultValue: "君の 夢は うららかに"))
+                }
+            }
+#endif
         }
         .formStyle(.grouped)
+#if false
+        .onKeyPress(keys: [.upArrow, .downArrow, .leftArrow, .rightArrow, .init("b"), .init("a")]) {
+            guard !shouldPresentAdditionalSettings else { return .ignored }
+            
+            if $0.key == Self.KEYSTROKES[additionalSettingsGuardIndex] {
+                additionalSettingsGuardIndex += 1
+                if additionalSettingsGuardIndex >= Self.KEYSTROKES.count {
+                    shouldPresentAdditionalSettings = true
+                    additionalSettingsGuardIndex = 0
+                }
+            } else {
+                additionalSettingsGuardIndex = 0
+            }
+            
+            return .handled
+        }
+#endif
     }
 }
 
