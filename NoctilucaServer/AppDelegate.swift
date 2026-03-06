dac3c299ea01bea649a34d38c7169516d4c61e9d
Gyuhwan Park★ <unstabler@unstabler.pl>
2026-03-07 01:56:02 +0900

    ui: 메인 메뉴에 Edit 메뉴 추가 (텍스트 입력 단축키 지원)

--- NoctilucaServer/AppDelegate.swift
+++ NoctilucaServer/AppDelegate.swift
@@ -63,6 +63,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         
         UNUserNotificationCenter.current().delegate = self
         
+        setupMainMenu()
         setupStatusItem()
         bindServerState()
         updateMenuState()
@@ -179,6 +180,27 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     @objc
     func quitApplication(_ sender: Any?) {
         NSApp.terminate(nil)
+    }
+
+    private func setupMainMenu() {
+        let mainMenu = NSMenu()
+
+        let editMenuItem = NSMenuItem()
+        let editMenu = NSMenu(title: "Edit")
+
+        editMenu.addItem(withTitle: "Undo", action: Selector(("undo:")), keyEquivalent: "z")
+        editMenu.addItem(withTitle: "Redo", action: Selector(("redo:")), keyEquivalent: "Z")
+        editMenu.addItem(.separator())
+        editMenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
+        editMenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
+        editMenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
+        editMenu.addItem(withTitle: "Delete", action: #selector(NSText.delete(_:)), keyEquivalent: "\u{8}")
+        editMenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
+
+        editMenuItem.submenu = editMenu
+        mainMenu.addItem(editMenuItem)
+
+        NSApp.mainMenu = mainMenu
     }
 
     private func setupStatusItem() {
