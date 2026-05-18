06a370dddeb4476ff0352703191841b6c8a3003c
Gyuhwan Park★ <unstabler@unstabler.pl>
2026-05-18 11:52:18 +0900

    gesu: swift-syntax 의존성을 601.x 로 고정 (Shotoku 와 정렬)

--- Gesu/Package.swift
+++ Gesu/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0-latest"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "601.0.0"..<"602.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
